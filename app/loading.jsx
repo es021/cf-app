1cf55b99152e9dd4f683e8b661a213c83822b11a
es021 <zulsarhan.shaari@gmail.com>
2017-11-23 15:53:33 +0800

    add loading toogle

--- app/loading.jsx
+++ app/loading.jsx
@@ -3,7 +3,37 @@ import {render} from 'react-dom';
 
 require("./css/app-loading.scss");
 const AppLoading = () => (
-                <div>Loading</div>
+            <div>Loading</div>
             );
 
-render(<AppLoading/>, document.getElementById('app-loading'));
+//show loading until the main bundle finish load
+function loadScript(url, callback) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    if (script.readyState) {  //IE
+        script.onreadystatechange = function () {
+            if (script.readyState === "loaded" || script.readyState === "complete") {
+                script.onreadystatechange = null;
+                callback();
+            } 
+        };
+    } else {  //Others
+        script.onload = function () {
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("body")[0].appendChild(script);
+}
+
+const app_load = document.getElementById('app-loading');
+
+var main = "asset/js/main.bundle.js";
+loadScript(main, function () {
+    console.log('script ready!');
+    //hide the loading
+    app_load.style["display"] = "none";
+});
+
+render(<AppLoading/>, app_load);
