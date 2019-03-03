3f0c09588720831a05702205b3a5cb2feffb9601
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2019-03-03 16:10:06 +0800

    A

---
+++ note/ok.js
@@ -0,0 +1,36 @@
+[{
+    "name": "National Assembly of Malaysian Students Association",
+    "shortname": "NAMSA",
+    "logo": ""
+}, {
+    "name": "US East Coast Presidential Council",
+    "shortname": "EPIC",
+    "logo": ""
+}, {
+    "name": "West Coast Council",
+    "shortname": "WCC",
+    "logo": ""
+}, {
+    "name": "Talent Corp Malaysia",
+    "shortname": "",
+    "logo": ""
+}, {
+    "name": "Council of Midwest Malaysian Students",
+    "shortname": "COMMS",
+    "logo": ""
+}, {
+    "name": "International Council of Malaysian Scholars and Associates",
+    "shortname": "ICMS",
+    "logo": ""
+}]
+
+
+
+[{
+    "name": "Talent Corp Malaysia",
+    "shortname": "",
+    "logo": "TalentCorp.jpg",
+    "url": "https://www.talentcorp.com.my/"
+}]
+
+
