123ea759e074e84218cafbb377800cbc7829d2f2
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2019-11-26 16:01:58 +0800

    A

--- app/redux/reducer/auth-reducer.js
+++ app/redux/reducer/auth-reducer.js
@@ -1,204 +1,206 @@
-import {
-    getNewState
-} from './_helper';
-import * as authActions from '../actions/auth-actions';
-import {
-    _GET
-} from '../../lib/util';
+import { getNewState } from "./_helper";
+import * as authActions from "../actions/auth-actions";
+import { _GET } from "../../lib/util";
 
 const CF_DEFAULT = authActions.getCFDefault();
+const IS_HAS_PARAM_CF = location.href.indexOf("?cf=") >= 0;
 
 var getCF = _GET("cf");
 
 const authReducerInitState = {
-    cf: null,
-    user: null,
-    isAuthorized: false,
-    fetching: false,
-    error: null,
-    cookie: true,
+  cf: null,
+  user: null,
+  isAuthorized: false,
+  fetching: false,
+  error: null,
+  cookie: true
 };
 
 var auth = null;
 var hasLocalStorageSupport;
 
 try {
-    window.localStorage.setItem("testKey", 'foo');
-    window.localStorage.removeItem("testKey");
-    hasLocalStorageSupport = true;
+  window.localStorage.setItem("testKey", "foo");
+  window.localStorage.removeItem("testKey");
+  hasLocalStorageSupport = true;
 } catch (e) {
-    console.log(e);
-    hasLocalStorageSupport = false;
+  console.log(e);
+  hasLocalStorageSupport = false;
 }
-
 
 const AUTH_LOCAL_STORAGE = "auth";
 
 function setAuthLocalStorage(newItem) {
-    if (!hasLocalStorageSupport) {
-        return;
+  if (!hasLocalStorageSupport) {
+    return;
+  }
+  var auth = JSON.parse(window.localStorage.getItem(AUTH_LOCAL_STORAGE));
+  if (auth !== null) {
+    for (var k in newItem) {
+      auth[k] = newItem[k];
     }
-    var auth = JSON.parse(window.localStorage.getItem(AUTH_LOCAL_STORAGE));
-    if (auth !== null) {
-        for (var k in newItem) {
-            auth[k] = newItem[k];
-        }
-    } else {
-        auth = newItem;
-    }
-    window.localStorage.setItem(AUTH_LOCAL_STORAGE, JSON.stringify(auth));
+  } else {
+    auth = newItem;
+  }
+  window.localStorage.setItem(AUTH_LOCAL_STORAGE, JSON.stringify(auth));
 }
 
 function clearAuthLocalStorage() {
-    if (hasLocalStorageSupport) {
-        window.localStorage.removeItem(AUTH_LOCAL_STORAGE);
-    }
+  if (hasLocalStorageSupport) {
+    window.localStorage.removeItem(AUTH_LOCAL_STORAGE);
+  }
 }
 
 function fixLocalStorageAuth(auth) {
-    if (!hasLocalStorageSupport) {
-        return;
-    }
+  if (!hasLocalStorageSupport) {
+    return;
+  }
 
-    if (typeof auth["user"] === "undefined" || typeof auth["isAuthorized"] === "undefined") {
-        return fixCFAuth(authReducerInitState);
-    }
+  if (
+    typeof auth["user"] === "undefined" ||
+    typeof auth["isAuthorized"] === "undefined"
+  ) {
+    return fixCFAuth(authReducerInitState);
+  }
 
-    auth = fixCFAuth(auth);
+  auth = fixCFAuth(auth);
 
-    // clear all possible error
-    auth["error"] = null;
-    auth["fetching"] = false;
+  // clear all possible error
+  auth["error"] = null;
+  auth["fetching"] = false;
 
-    return auth;
+  return auth;
 }
 
 // the cf param will force to the cf
 // use in logout
 function fixCFAuth(auth, cf = null) {
-    if (cf !== null) {
-        auth["cf"] = cf;
-        return auth;
-    }
-
-    // if get is changing and is not authorized
-    if ((!auth.isAuthorized || typeof auth["isAuthorized"] === "undefined")) {
-        if (getCF !== null) {
-            auth["cf"] = getCF;
-        }
-    }
-
-    // if still null get default
-    if (auth["cf"] == null) {
-        auth["cf"] = CF_DEFAULT;
-    }
-
+  if (cf !== null) {
+    auth["cf"] = cf;
     return auth;
+  }
+
+  // if get is changing and is not authorized
+  if (!auth.isAuthorized || typeof auth["isAuthorized"] === "undefined") {
+    if (getCF !== null) {
+      auth["cf"] = getCF;
+    }
+  }
+
+  // if still null get default
+  if (auth["cf"] == null) {
+    auth["cf"] = CF_DEFAULT;
+  }
+
+  // fix untuk amik yang default kalau takde param cf kat url
+  if (!IS_HAS_PARAM_CF) {
+    auth["cf"] = CF_DEFAULT;
+  }
+
+  return auth;
 }
 
 function autoLogoutIfCFChange(curAuth) {
-    if (getCF !== null && getCF != curAuth.cf) {
-        curAuth.isAuthorized = false;
-        curAuth.cf = getCF;
-    }
-    return curAuth;
+  if (getCF !== null && getCF != curAuth.cf) {
+    curAuth.isAuthorized = false;
+    curAuth.cf = getCF;
+  }
+  return curAuth;
 }
 
 if (hasLocalStorageSupport) {
-    auth = window.localStorage.getItem(AUTH_LOCAL_STORAGE);
+  auth = window.localStorage.getItem(AUTH_LOCAL_STORAGE);
 
-    if (auth !== null) {
-        auth = JSON.parse(auth);
-        auth = getNewState(auth, {
-            cookie: true
-        });
-        auth = fixLocalStorageAuth(auth);
-    } else {
-        auth = authReducerInitState;
-        setAuthLocalStorage(authReducerInitState);
-    }
+  if (auth !== null) {
+    auth = JSON.parse(auth);
+    auth = getNewState(auth, {
+      cookie: true
+    });
+    auth = fixLocalStorageAuth(auth);
+  } else {
+    auth = authReducerInitState;
+    setAuthLocalStorage(authReducerInitState);
+  }
 
-    auth = fixCFAuth(auth);
-    auth = autoLogoutIfCFChange(auth);
-    setAuthLocalStorage(auth);
+  auth = fixCFAuth(auth);
+  auth = autoLogoutIfCFChange(auth);
+  setAuthLocalStorage(auth);
 }
 // need to find a fallback for safari
 else {
-    //    auth = {
-    //
-    //        user: {
-    //            ID: 136,
-    //            user_status: "Active",
-    //            first_name: "Student",
-    //            last_name: "For Test",
-    //            img_url: "http://seedsjobfairapp.com/wp-content/uploads/2017/07/user_136_profile_image.jpeg",
-    //            img_pos: "18% 29%",
-    //            img_size: "195% auto"
-    //        },
-    //
-    //        isAuthorized: true,
-    //        fetching: false,
-    //        error: null
-    //    };
+  //    auth = {
+  //
+  //        user: {
+  //            ID: 136,
+  //            user_status: "Active",
+  //            first_name: "Student",
+  //            last_name: "For Test",
+  //            img_url: "http://seedsjobfairapp.com/wp-content/uploads/2017/07/user_136_profile_image.jpeg",
+  //            img_pos: "18% 29%",
+  //            img_size: "195% auto"
+  //        },
+  //
+  //        isAuthorized: true,
+  //        fetching: false,
+  //        error: null
+  //    };
 
-    auth = getNewState(authReducerInitState, {
-        cookie: false
-    });
-    auth = fixCFAuth(auth);
+  auth = getNewState(authReducerInitState, {
+    cookie: false
+  });
+  auth = fixCFAuth(auth);
 }
 
-
 export default function authReducer(state = auth, action) {
-    switch (action.type) {
-        case authActions.UPDATE_USER: {
-            var newUser = getNewState(state.user, action.payload);
-            var newState = {
-                user: newUser
-            };
+  switch (action.type) {
+    case authActions.UPDATE_USER: {
+      var newUser = getNewState(state.user, action.payload);
+      var newState = {
+        user: newUser
+      };
 
-            setAuthLocalStorage(newState);
-            return getNewState(state, newState);
-        }
-        case authActions.DO_LOGOUT: {
-            clearAuthLocalStorage();
-            return getNewState(state, fixCFAuth(authReducerInitState, state.cf));
-        }
-        case authActions.DO_LOGIN + '_PENDING': {
-            var newState = {
-                fetching: true,
-                error: null
-            };
-
-            setAuthLocalStorage(newState);
-            return getNewState(state, newState);
-        }
-        case authActions.DO_LOGIN + '_FULFILLED': {
-
-            var user = action.payload.data;
-            var newState = {
-                cf: user.cf,
-                fetching: false,
-                isAuthorized: (state.cookie) ? true : false,
-                user: user,
-                error: null
-            };
-
-            setAuthLocalStorage(newState);
-            return getNewState(state, newState);
-        }
-        case authActions.DO_LOGIN + '_REJECTED': {
-            var err = action.payload.response.data;
-
-            var newState = {
-                fetching: false,
-                isAuthorized: false,
-                error: err
-            };
-
-            setAuthLocalStorage(newState);
-            return getNewState(state, newState);
-        }
+      setAuthLocalStorage(newState);
+      return getNewState(state, newState);
     }
+    case authActions.DO_LOGOUT: {
+      clearAuthLocalStorage();
+      return getNewState(state, fixCFAuth(authReducerInitState, state.cf));
+    }
+    case authActions.DO_LOGIN + "_PENDING": {
+      var newState = {
+        fetching: true,
+        error: null
+      };
 
-    return state;
-};
+      setAuthLocalStorage(newState);
+      return getNewState(state, newState);
+    }
+    case authActions.DO_LOGIN + "_FULFILLED": {
+      var user = action.payload.data;
+      var newState = {
+        cf: user.cf,
+        fetching: false,
+        isAuthorized: state.cookie ? true : false,
+        user: user,
+        error: null
+      };
+
+      setAuthLocalStorage(newState);
+      return getNewState(state, newState);
+    }
+    case authActions.DO_LOGIN + "_REJECTED": {
+      var err = action.payload.response.data;
+
+      var newState = {
+        fetching: false,
+        isAuthorized: false,
+        error: err
+      };
+
+      setAuthLocalStorage(newState);
+      return getNewState(state, newState);
+    }
+  }
+
+  return state;
+}
--- note/other/ok2.js
+++ note/other/ok2.js
@@ -1,97 +1,143 @@
-21
-112
-117
-118
-121
-131
-150
-155
-165
-167
-174
-217
-244
-257
-273
-280
-292
-293
-309
-312
-371
-399
-416
-458
-483
-496
-639
-646
-651
-682
-688
-698
-700
-707
-708
-716
-726
-731
-745
-757
-759
-782
-783
-846
-1218
-1219
-1419
-1441
-1460
-1471
-1484
-1493
-1607
-1608
-1610
-1612
-1631
-1635
-1645
-1672
-1673
-1688
-1689
-1690
-1696
-1698
-1700
-1703
-1704
-1706
-1708
-1711
-1713
-1714
-1264
-1282
-1606
-1609
-1611
-1674
-1686
-1687
-1691
-1692
-1693
-1694
-1695
-1697
-1699
-1701
-1702
-1705
-1709
-1710
-1712
-1715
-1716
+a = `AirAsia	izzatularisya97@gmail.com		10:00:00
+AirAsia	shahrulidhamrusdi@gmail.com		10:20:00
+AirAsia	adekklienda@gmail.com		10:40:00
+AirAsia	farisazrai@gmail.com		11:00:00
+AirAsia	althonylxy-wa16@student.tarc.edu.my		11:20:00
+AirAsia	lim.weiwen1997@gmail.com		11:40:00
+AirAsia	nammohammed2@live.utm.my		12:40:00
+AirAsia	darysyinikumaresan@graduate.utm.my		15:00:00
+AirAsia	kelly97_ing@hotmail.com		15:20:00
+AirAsia	maizatulhusna01@gmail.com		15:40:00
+AirAsia	salmazamrin@gmail.com		13:40:00
+AirAsia	hadi.amin95@gmail.com		11:00:00
+AirAsia	litian0718@gmail.com		11:40:00
+AirAsia	mrdhiah1998@gmail.com		12:00:00
+AirAsia	danishmirul98@gmail.com		12:20:00
+AirAsia	nadiaasrin98@gmail.com		12:40:00
+AirAsia	yenping97@hotmail.com		13:00:00
+AirAsia	yongjinjoel@gmail.com		13:20:00
+AirAsia	niknurarisha@gmail.com		14:00:00
+AirAsia	noorfaraheliana1997@gmail.com		14:20:00
+AirAsia	ashafyyah@outlook.com		14:40:00
+CIMB	cmx235@gmail.com		11:00:00
+CIMB	chrischuax97@hotmail.com		11:20:00
+CIMB	vivianleong2602@gmail.com		11:20:00
+CIMB	shah.rymie@gmail.com		11:00:00
+Dimension Data (NTT)	litian0718@gmail.com		10:00:00
+Dimension Data (NTT)	mingching97@gmail.com		10:20:00
+Dimension Data (NTT)	chenlongtoh@gmail.com		10:40:00
+Dimension Data (NTT)	angkewei@gmail.com		11:00:00
+Dimension Data (NTT)	ExtremeDie@1utar.my		11:20:40
+Dimension Data (NTT)	acelimcw@hotmail.com		11:40:00
+Dimension Data (NTT)	lim.weiwen1997@gmail.com		12:00:00
+Dimension Data (NTT)	beatrice.peiying@gmail.com		12:20:00
+Dimension Data (NTT)	yongjinjoel@gmail.com		12:40:00
+Dimension Data (NTT)	w.shaher@graduate.utm.my		13:00:00
+Dimension Data (NTT)	wsh_97@outlook.com		13:20:00
+Dimension Data (NTT)	andy22_9907@hotmail.com		13:40:00
+Dimension Data (NTT)	shanggavee2@gmail.com		14:20:00
+Dimension Data (NTT)	darysyinikumaresan@graduate.utm.my		14:40:00
+Dimension Data (NTT)	autumn9827@gmail.com		15:00:00
+Maybank	shah.rymie@gmail.com		10:00:00
+Maybank	farisazrai@gmail.com		10:40:00
+Maybank	bamieeees.superstrong@gmail.com		10:20:00
+Maybank	ezzuddinn@gmail.com		11:00:00
+Maybank	benjaminzahn99@gmail.com		11:20:00
+Maybank	hadi.amin95@gmail.com		11:40:00
+Maybank	chrischuax97@hotmail.com		12:00:00
+Maybank	danishmirul98@gmail.com		13:00:00
+Maybank	thiahusna96@gmail.com		13:20:00
+Maybank	afif1998@graduate.utm.my		13:40:00
+Maybank	hakimhambali77@gmail.com		14:40:00
+Maybank	alyaizzaty91@yahoo.com		15:20:00
+Aemulus	wcfan0306@gmail.com		10:00:00
+Aemulus	yeefulian@1utar.my		10:20:00
+Aemulus	ExtremeDie@1utar.my		10:40:00
+Aemulus	ngjiunn1998@gmail.com		11:00:00
+Aemulus	litian0718@gmail.com		11:20:00
+Aemulus	althonylxy-wa16@student.tarc.edu.my		12:00:00
+Aemulus	chrischuax97@hotmail.com		12:20:00
+Aemulus	danial.jefri.7@gmail.com		14:20:00
+Kian Joo Group	mingching97@gmail.com		10:00:00
+Kian Joo Group	keensumkong@1utar.my		10:20:00
+Kian Joo Group	chenlongtoh@gmail.com		10:40:00
+Kian Joo Group	angkewei@gmail.com		11:00:00
+Kian Joo Group	ExtremeDie@1utar.my		11:20:00
+Kian Joo Group	acelimcw@hotmail.com		11:40:00
+Kian Joo Group	lim.weiwen1997@gmail.com		12:00:00
+Kian Joo Group	beatrice.peiying@gmail.com		12:20:00
+Kian Joo Group	yongjinjoel@gmail.com		12:40:00
+Kian Joo Group	w.shaher@graduate.utm.my		13:00:00
+Kian Joo Group	wsh_97@outlook.com		13:20:00
+Kian Joo Group	andy22_9907@hotmail.com		13:40:00
+Kian Joo Group	shanggavee2@gmail.com		14:20:00
+Kian Joo Group	darysyinikumaresan@graduate.utm.my		14:40:00
+Kian Joo Group	autumn9827@gmail.com		15:00:00
+Top Glove	tlter@yahoo.com		10:00:00
+Top Glove	mqayyum1223@gmail.com		10:20:00
+Top Glove	fatin.nurbaya08@gmail.com		10:40:00
+Top Glove	anazri.amz@gmail.com		11:00:00
+Top Glove	nurshafira321@gmail.com		11:20:00
+Top Glove	angkewei@gmail.com		11:40:00
+Top Glove	heyimzue@gmail.com		12:00:00
+Top Glove	qamarena.suhaimi@gmail.com		12:20:00
+Top Glove	anissyahira.zulkefli@gmail.com		12:40:00
+Top Glove	beatrice.peiying@gmail.com		13:00:00
+Top Glove	azriamirah.my@gmail.com		13:20:00
+Top Glove	ibnuidris.farhan@gmail.com		13:40:00
+Top Glove	hakimhambali77@gmail.com		14:00:00
+Top Glove	afif1998@graduate.utm.my		14:20:00
+Top Glove	shanggavee2@gmail.com		14:40:00
+Top Glove	nmssyahmi@gmail.com		15:00:00
+Top Glove	darysyinikumaresan@graduate.utm.my		15:20:00
+Top Glove	mrdhiah1998@gmail.com		15:40:00
+Top Glove	fatinnadhira25@gmail.com		16:00:00`;
+
+a = `Kian Joo Group	mingching97@gmail.com		10:00:00
+Kian Joo Group	keensumkong@1utar.my		10:20:00
+Kian Joo Group	yongjinjoel@gmail.com		11:00:00
+Kian Joo Group	nabilanajwaaziz@gmail.com		11:20:00
+Kian Joo Group	beatrice.peiying@gmail.com		11:40:00
+Kian Joo Group	ExtremeDie@1utar.my		12:00:00
+Kian Joo Group	lim.weiwen1997@gmail.com		12:20:00
+Kian Joo Group	mirzamohid1001@gmail.com		12:40:00
+Kian Joo Group	ina21hana2@gmail.com		13:00:00
+Kian Joo Group	niknurarisha@gmail.com		13:20:00
+Kian Joo Group	chenlongtoh@gmail.com		13:40:00
+Kian Joo Group	eyqashaf@gmail.com		14:00:00
+Kian Joo Group	akmalhakimakmalhakim@gmail.com		14:20:00
+Kian Joo Group	shanggavee2@gmail.com		15:00:00
+Kian Joo Group	fatinnadhira25@gmail.com		15:20:00
+Kian Joo Group	darysyinikumaresan@graduate.utm.my		15:40:00
+Kian Joo Group	maizatulhusna01@gmail.com		16:00:00`
+
+a = a.split("\n");
+SS = ``;
+for (var i in a) {
+  let row = a[i];
+  row = row.split("\t");
+  let com = row[0];
+  let email = row[1];
+  let date = "11/27/2019";
+  let time = row[3];
+
+  var d = Date.parse(`${date} ${time}`);
+  d = d.toString() / 1000;
+
+  console.log(com, email, date, time, d);
+
+  let s = `
+    insert into pre_screens (student_id, company_id, status, special_type, appointment_time, is_onsite_call , updated_by)
+    select u.ID as student_id, c.ID as company_id, '2_Approved' as status, 'Student Profile' as special_type, 
+    ${d} as appointment_time, 1 as is_onsite_call , 136 as updated_by
+    from wp_cf_users u, companies c 
+        where 1=1
+        and c.name='${com}' 
+        and u.user_email='${email}' ;
+
+    `;
+
+  SS += s;
+}
+
+console.log(SS);
