4980edef246a1312ab394235f019734046daa886
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2020-03-13 22:34:07 +0800

    A

---
+++ config/custom-cf-config.js
@@ -0,0 +1,16 @@
+const Feature = {
+    HOME_GALLERY : "HOME_GALLERY",
+    COMPANY_BOOTH : "COMPANY_BOOTH"
+}
+    
+const Style = {
+    HEADER_ICON : "HEADER_ICON",
+    HEADER_ICON_URL : "HEADER_ICON_URL",
+    HEADER_DESC : "HEADER_DESC"
+
+}
+
+module.exports = {
+    Feature,
+    Style
+};
