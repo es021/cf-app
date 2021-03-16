964a4e4a048842d77cd6d0b59ff2269ac825fb8d
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2021-03-16 16:43:20 +0800

    a

--- server/model/browse-student-query.js
+++ server/model/browse-student-query.js
@@ -191,7 +191,8 @@ class BrowseStudentExec {
 			let additional_where = "1=1";
 			if (key == "looking_for_position") {
 				// additional_where = " s.val not like '%part%' ";
-				additional_where = " s.val IN (select vv.val FROM ref_looking_for_position vv) ";
+				// additional_where = " s.val IN (select vv.val FROM ref_looking_for_position vv) ";
+				additional_where = " s.val IN ('Full-Time', 'Internship') ";
 			}
 
 			return `select 
