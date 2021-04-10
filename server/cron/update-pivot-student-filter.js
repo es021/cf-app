abc4c6bc3615c7ac751179f43516263f62d1cbdd
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2021-04-10 10:14:17 +0800

    a

--- server/cron/update-pivot-student-filter.js
+++ server/cron/update-pivot-student-filter.js
@@ -2,24 +2,36 @@ const { graphql } = require("../../helper/api-helper");
 const DB = require("../model/DB");
 
 function Main() {
+    var toLoad = 0;
+    var loaded = 0;
+    function finish(success) {
+        loaded++;
+        console.log(`done ${loaded}/${toLoad}`)
+        if (loaded >= toLoad) {
+            process.exit()
+        }
+    }
+
     DB.query("SELECT param FROM pivot_student_filter").then(res => {
+        toLoad = res.length;
         for (let r of res) {
             let param = r.param;
             try {
                 param = JSON.parse(param);
                 console.log(param);
-
                 let q = `query{ 
                     browse_student_filter( 
                         override_pivot: true,
-                        current_cf:"TEST", 
-                        discard_filter:"::interested_job_location::::university::" 
+                        current_cf:"${param.current_cf}", 
+                        discard_filter:"${param.discard_filter}" 
                     ) 
                     { _key _val _total _val_label } 
                 }`;
                 graphql(q)
                     .then(res => {
-                        console.log(res.data.data);
+                        finish(true);
+                    }).err(err => {
+                        finish(false);
                     })
             } catch (err) { }
         }
