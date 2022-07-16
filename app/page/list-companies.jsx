49dc18e019743cf36c620f998cb45c68f110c185
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2022-07-16 23:11:49 +0800

    a

--- app/page/list-companies.jsx
+++ app/page/list-companies.jsx
@@ -7,14 +7,24 @@ import { CFSMeta } from "../../config/db-config";
 export default class ListCompanies extends React.Component {
   constructor(props) {
     super(props);
+    this.defaultOffset = 15;
+    this.offset = 15;
+
+    let offsetCompany = getCfCustomMeta(CFSMeta.OFFSET_LOAD_COMPANY_LISTING, this.defaultOffset)
+    offsetCompany = Number.parseInt(offsetCompany);
+    if (isNaN(offsetCompany)) {
+      offsetCompany = this.defaultOffset;
+    }
+    this.offset = offsetCompany;
   }
+
 
   render() {
     let companySingle = getCfCustomMeta(CFSMeta.TEXT_COMPANY_ENTITY_SINGLE, "Company");
     let profileTerm = getCfCustomMeta(CFSMeta.TEXT_COMPANY_PROFILE_TERM, "Profiles");
     return <div >
       <h1>{companySingle} {profileTerm}</h1>
-      <CompaniesSection {...this.props} offset={15} />
+      <CompaniesSection {...this.props} offset={this.offset} />
     </div >
   }
 }
